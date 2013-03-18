6674c09d207896cf6758ecaed6f9918d44bb6d1f
Nick Shvelidze <shveloo@gmail.com>
2013-03-18 22:12:16 +0400

    Update

--- translate.rb
+++ translate.rb
@@ -10,42 +10,69 @@ doc = Nokogiri::XML file
 
 notes = []
 durations = []
+delays = []
 
 i = 0
 
-doc.search('note').each do |note|
-	break if i > 2000
+divisions = doc.search("divisions").inner_text.to_i
 
-	note_name = note.search("step").inner_text
-	note_name += "S" if note.search("alter").inner_text == "1"
-	note_name += note.search("octave").inner_text
-	if note_name.length > 0 then
-		notes.push note_name.upcase
-		
-		x_duration = note.search("duration").inner_text
-
-		type = note.search("type").inner_text
-		case type
-			when "whole"
-				duration = 1000
-			when "half"
-				duration = 1000 / 2
-			when "quarter"
-				duration = 1000 / 4
-			when "eighth"
-				duration = 1000 / 8
-			else
-				duration = 1000
-		end
-		
-		duration = x_duration if x_duration.to_i < duration.to_i
-
-		durations.push duration.to_s
-	end
-
-	i += 1
+sound = doc.search("sound")
+if sound.first then
+	tempo = sound.first.attribute("tempo").to_i
+else
+	tempo = 120
 end
 
+quarter_note = 60.0  / tempo / divisions * 1000.0
+
+delay = 0
+
+doc.search('measure').each do |measure|
+
+	rest = measure.css("rest").first
+
+	if rest then
+		delay = rest.parent.search("duration").inner_text.to_i
+		delay = delay * quarter_note
+	end
+
+	measure.search('note').each do |note|
+		break if i > 2000
+
+		note_name = note.search("step").inner_text
+		note_name += "S" if note.search("alter").inner_text == "1"
+		note_name += note.search("octave").inner_text
+		if note_name.length > 0 then
+			notes.push note_name.upcase
+			
+			duration = note.search("duration").inner_text
+			if duration.length > 0 then
+				duration = duration.to_i * quarter_note
+			else
+				type = note.search("type").inner_text
+				case type
+					when "whole"
+						duration = quarter_note * 4
+					when "half"
+						duration = quarter_note * 2
+					when "quarter"
+						duration = quarter_note
+					when "eighth"
+						duration = quarter_note / 2.0
+					else
+						duration = quarter_note * 4
+				end
+			end
+			
+			durations.push duration.to_s
+
+			delays.push delay.to_s
+		end
+
+		i += 1
+	end
+
+end
 
 
 output = "int notes[] = {"
@@ -61,15 +88,14 @@ durations.each do |duration|
 end
 output += "};\n"
 
+output += "int delays[] = {"
+delays.each do |delay|
+	delay = "0" if delay.to_i < 1
+	output += delay + ","
+end
+output += "};\n"
+
 output += "int melody_size = #{notes.length};\n"
-
-time = doc.search "time"
-
-beats = time.search("beats").inner_text.to_i
-beat_type = time.search("beat-type").inner_text.to_i
-
-pause_mult = (1.0 / beats) + 1
-output += "float pause_mult = #{pause_mult};\n"
 
 out = File.open "melody.txt", "w"
 out.write output
